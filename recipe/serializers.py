56e8da652311545d7b51f03bcbdc43a7f28df7bf
Trương Quang Chứ <75611709+tqchu@users.noreply.github.com>
2023-05-17 13:28:59 +0700

    Done sorting and implementing searching

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.relations import PrimaryKeyRelatedField, StringRelatedField
 from rest_framework.serializers import ModelSerializer
@@ -19,19 +20,11 @@ class ReviewSerializer(ModelSerializer):
         fields = ('id','content','last_edited', 'rating', 'recipe', 'username','user_image')
 
 class RecipeSerializer(ModelSerializer):
-    rating = SerializerMethodField()
-    author = StringRelatedField(source='user.username', read_only=True)
+    author = serializers.CharField()
+    average_rating = serializers.FloatField()
     class Meta:
         model = Recipe
-        fields = ('id','title','calories', 'cooking_time', 'image', 'rating', 'author')
-
-    def get_rating(self, obj):
-        reviews = obj.reviews.all()
-        if reviews:
-            total_rating = sum(review.rating for review in reviews)
-            return total_rating / len(reviews)
-        else:
-            return None
+        fields = ('id','title','calories', 'cooking_time', 'image', 'average_rating', 'author')
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -39,4 +32,4 @@ class RecipeDetailSerializer(RecipeSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
     class Meta:
         model = Recipe
-        fields = ('id', 'title','ingredients', 'reviews','directions', 'calories', 'cooking_time', 'last_edited', 'image','rating', 'author')
+        fields = ('id', 'title','ingredients', 'reviews','directions', 'calories', 'cooking_time', 'last_edited', 'image','average_rating', 'author')
