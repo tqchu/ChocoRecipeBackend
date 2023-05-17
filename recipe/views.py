45a8d47d8e1e32b00c8d049f3c7d4357f58a19bf
Trương Quang Chứ <75611709+tqchu@users.noreply.github.com>
2023-05-17 13:28:59 +0700

    Done sorting and implementing searching

--- recipe/views.py
+++ recipe/views.py
@@ -1,8 +1,12 @@
+from django.db.models import Avg, F
+from django.db.models import Q
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from recipe.models import Recipe
 from recipe.serializers import RecipeDetailSerializer, RecipeSerializer
+from rest_framework import filters
 
 
 # Create your views here
@@ -12,26 +16,41 @@ from recipe.serializers import RecipeDetailSerializer, RecipeSerializer
 
 def parse_sort_field(sort_field):
     sort_fields = {
-        'rating': 'rating',
-        'like': 'like',
-        'calories': 'calories',
+        'rating': '-average_rating',
+        'like': '-like',
+        'calories': '-calories',
         'quick': 'cooking_time',
     }
     return sort_fields[sort_field]
+
+
 class RecipeList(APIView):
     def get(self, request):
-        params = request.query_params
-        if 'sortBy' in params:
-            sort_field = parse_sort_field(params['sortBy'])
+        search = request.query_params.get('keyword')
+        ordering = request.query_params.get('sortBy')
+        if ordering:
+            sort_field = parse_sort_field(ordering)
         else:
             sort_field = 'id'
-        recipes = Recipe.objects.all().order_by(sort_field)
+        if search:
+            recipes = Recipe.objects.annotate(
+                author=F('user__username')
+            ).filter(
+                Q(directions__icontains=search) | Q(title__icontains=search) | Q(author__icontains=search)).annotate(
+                average_rating=Avg('reviews__rating')).order_by(sort_field)
+        else:
+            recipes = Recipe.objects.annotate(
+                author=F('user__username')
+            ).annotate(average_rating=Avg('reviews__rating')).order_by(sort_field)
+
         serializer = RecipeSerializer(recipes, many=True)
         return Response(serializer.data)
+
 
 class RecipeDetail(APIView):
     def get(self, request, pk):
         recipe = Recipe.objects.get(pk=pk)
+        recipe.average_rating = recipe.reviews.aggregate(Avg('rating'))['rating__avg']
         serializer = RecipeDetailSerializer(recipe)
         return Response(serializer.data)
         # return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED
