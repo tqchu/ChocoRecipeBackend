6397c242e984bb0e3cdfa23ecc11ea4e21f6514f
HoangTrong <hoangcool0988@gmail.com>
2023-05-17 17:29:58 +0700

    resolve conflict

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -9,7 +9,7 @@ class IngredientSerializer(ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('name','quantity','unit')
-
+    
 
 class RecipeSerializer(ModelSerializer):
     author = serializers.CharField()
@@ -25,3 +25,52 @@ class RecipeDetailSerializer(RecipeSerializer):
     class Meta:
         model = Recipe
         fields = ('id', 'title','ingredients', 'reviews','directions', 'calories', 'cooking_time', 'last_edited', 'image','average_rating', 'author')
+
+
+class CreateRecipeSerializer(RecipeSerializer):
+    ingredients = IngredientSerializer(many=True)
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Recipe
+        fields = ('id','user_id','title', 'directions', 'calories', 'cooking_time','image', 'ingredients')
+        extra_kwargs = {
+            'title': {'required': True},
+            'directions': {'required': True},
+            'calories': {'required': True}
+        }
+
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
+        for ingredient in ingredients:
+            Ingredient.objects.create(recipe=recipe, **ingredient)
+        return recipe
+    
+    
+    def update(self,instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.directions = validated_data.get('directions', instance.directions)
+        instance.calories = validated_data.get('calories', instance.calories)
+        instance.cooking_time = validated_data.get('cooking_time', instance.cooking_time)
+        instance.image = validated_data.get('image',instance.image)
+
+        ingredients_data = validated_data.get('ingredients')
+        for ingredient_data in ingredients_data:
+            ingredient_id = ingredient_data.get('id')
+            if ingredient_id:
+                # update existing ingredient
+                ingredient = Ingredient.objects.get(id=ingredient_id)
+                ingredient.name = ingredient_data.get('name', ingredient.name)
+                ingredient.quantity = ingredient_data.get('quantity', ingredient.quantity)
+                ingredient.unit = ingredient_data.get('unit', ingredient.unit)
+                ingredient.save()
+            else:
+                # create new ingredient
+                Ingredient.objects.create(recipe=instance, **ingredient_data)
+
+        instance.save()
+        return instance
+    
+    def delete(self,instance):
+        instance.delete()
--- recipe/views.py
+++ recipe/views.py
@@ -2,10 +2,11 @@ from django.db.models import Avg, F
 from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from recipe.models import Recipe
-from recipe.serializers import RecipeDetailSerializer, RecipeSerializer
-
+from recipe.models import Ingredient, Recipe
+from recipe.serializers import CreateRecipeSerializer, RecipeDetailSerializer, RecipeSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
 
 # Create your views here
 #
@@ -23,6 +24,13 @@ def parse_sort_field(sort_field):
 
 
 class RecipeList(APIView):
+    permission_classes = [IsAuthenticated]
+    def check_permissions(self, request):
+
+        if (request.method == 'POST' or request.method == "PUT" or request.method == "DELETE") and not request.user.is_authenticated:
+            raise PermissionDenied()
+        super().check_permissions(request)
+
     def get(self, request):
         search = request.query_params.get('keyword')
         ordering = request.query_params.get('sort_by')
@@ -50,6 +58,85 @@ class RecipeList(APIView):
         serializer = RecipeSerializer(recipes, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CreateRecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            user_id = serializer.validated_data.get('user_id')
+            if user_id != request.user.id:
+                return Response({'error': 'You are not authorized to create a recipe for another user.'},
+                                status=status.HTTP_403_FORBIDDEN)
+
+            recipe = Recipe.objects.create(
+                user=request.user,
+                title=serializer.validated_data.get('title'),
+                directions=serializer.validated_data.get('directions'),
+                calories=serializer.validated_data.get('calories'),
+                cooking_time=serializer.validated_data.get('cooking_time'),
+                image=serializer.validated_data.get('image')
+            )
+
+            # Create a list of ingredient objects and add them to the new recipe
+            ingredients_data = serializer.validated_data.get('ingredients')
+            for ingredient_data in ingredients_data:
+                Ingredient.objects.create(
+                    recipe=recipe,
+                    name=ingredient_data.get('name'),
+                    quantity=ingredient_data.get('quantity'),
+                    unit=ingredient_data.get('unit')
+                )
+
+            serializer = CreateRecipeSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self,request):
+        id = request.data.get('id')
+        try:
+            recipe = Recipe.objects.get(id=id)
+        except Recipe.DoesNotExist:
+            return Response({'error': 'Recipe does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CreateRecipeSerializer(recipe, data=request.data)
+        if serializer.is_valid():
+            user_id = serializer.validated_data.get('user_id')
+            if user_id != request.user.id:
+                return Response({'error': 'You are not authorized to update a recipe for another user.'},
+                                status=status.HTTP_403_FORBIDDEN)
+
+            recipe = serializer.save()
+
+            # Update the list of ingredient objects for the recipe
+            Ingredient.objects.filter(recipe=recipe).delete()
+            ingredients_data = serializer.validated_data.get('ingredients')
+            for ingredient_data in ingredients_data:
+                Ingredient.objects.create(
+                    recipe=recipe,
+                    name=ingredient_data.get('name'),
+                    quantity=ingredient_data.get('quantity'),
+                    unit=ingredient_data.get('unit')
+                )
+
+            serializer = CreateRecipeSerializer(recipe)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request):
+        id = request.data.get('id')
+        user_id = request.data.get('user_id')
+        try:
+            recipe = Recipe.objects.get(id=id)
+        except Recipe.DoesNotExist:
+            return Response({'error': 'Recipe does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = CreateRecipeSerializer(recipe)
+        
+        if user_id != request.user.id:
+            return Response({'error': 'You are not authorized to update a recipe for another user.'},
+                                status=status.HTTP_403_FORBIDDEN)
+        serializer.delete(recipe)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+        
 class RecipeDetail(APIView):
     def get(self, request, pk):
         recipe = Recipe.objects.get(pk=pk)
